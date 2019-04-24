c9520560af5abff1e2881e8791c6ab8dd004d94b
rvoss2 <49959404+rvoss2@users.noreply.github.com>
2019-04-24 13:52:04 -0500

    Add files via upload

---
+++ PackageViewer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Housing.Core.BLL
+{
+    /// <summary>
+    /// Business class responsible viewing Package view models
+    /// </summary>
+    class PackageViewer
+    {
+        private readonly PackageDAO _packageDAO;
+
+        public PackageViewer()
+        {
+            _packageDAO = new PackageDAO();
+        }
+
+        /// <summary>
+        /// Returns specified package?
+        /// </summary>
+        public string GetStudent(long PackageID) //
+        {
+            return _packageDAO.GetPackage(PackageID);
+        }
+    }
+}
